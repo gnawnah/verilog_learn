f600f4e8acf933f5fbbb0ca2ce54c326b94be2f9
gnawnah <hwan477@aucklanduni.ac.nz>
2026-03-21 16:12:03 +1300

    keep going

---
+++ case.v
@@ -0,0 +1,40 @@
+`timescale 1us/1ns
+module case_statement ();
+
+reg r_VAL_1 = 1'b0;
+reg r_VAL_2 = 1'b0;
+reg r_VAL_3 = 1'b0;
+
+reg [3:0] r_RESULT = 4'b0000;
+
+always @(*)
+begin
+  case({r_VAL_1, r_VAL_2, r_VAL_3})
+  3'b000: r_RESULT <=0;
+  3'b001: r_RESULT <=1;
+  3'b010: r_RESULT <=2;
+  default: r_RESULT <=9;
+  endcase
+end
+
+initial begin
+  $dumpfile("case.vcd");
+  $dumpvars(0, case_statement);
+  r_VAL_1 <= 1'b0;
+  r_VAL_2 <= 1'b0;
+  r_VAL_3 <= 1'b0;
+  #100;
+  r_VAL_2 <= 1'b0;
+  r_VAL_3 <= 1'b1;
+  #100;
+  r_VAL_2 <= 1'b1;
+  r_VAL_3 <= 1'b0;
+  #100;
+  r_VAL_2 <= 1'b1;
+  r_VAL_3 <= 1'b1;
+  #100;
+  #1000000;
+  $finish;
+end
+
+endmodule
